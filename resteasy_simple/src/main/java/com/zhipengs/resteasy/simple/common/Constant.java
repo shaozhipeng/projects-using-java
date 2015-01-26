8429db887f2058a15aa2d14ce2ce96ba659394eb
shaozhipeng <zhipenglees@gmail.com>
2015-01-26 21:53:15 +0800

    complete project first time

---
+++ resteasy_simple/src/main/java/com/zhipengs/resteasy/simple/common/Constant.java
@@ -0,0 +1,13 @@
+package com.zhipengs.resteasy.simple.common;
+
+/**
+ * Contants
+ * 
+ * @author shaozhipeng
+ * 
+ */
+public class Constant {
+
+	public static final String HOSTURL = "http://localhost:8080/";
+
+}
---
+++ resteasy_simple/src/test/java/com/zhipengs/resteasy/simple/httpclient/AbstractHttpClientHandle.java
@@ -0,0 +1,29 @@
+package com.zhipengs.resteasy.simple.httpclient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+/**
+ * AbstractHttpClientHandle
+ * 
+ * @author shaozhipeng
+ * 
+ */
+public class AbstractHttpClientHandle {
+	protected static List<NameValuePair> transformMap(Map<String, String> params) {
+		// 如果参数为空则返回null;
+		if (params == null || params.size() < 0) {
+			return new ArrayList<NameValuePair>();
+		}
+
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		for (Map.Entry<String, String> map : params.entrySet()) {
+			paramList.add(new BasicNameValuePair(map.getKey(), map.getValue()));
+		}
+		return paramList;
+	}
+}
